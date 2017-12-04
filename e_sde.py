8534e2fab731b89ca892e81b1e829388f33feed5
Raquel Alvarez <raquelalvarez@Raquels-MacBook-Pro.local>
2017-12-04 04:03:26 -0500

    adding finalized scripts

--- e_sde.py
+++ e_sde.py
@@ -20,7 +20,8 @@ def arp_reply_intercept_esde(pkt):
 		mac_src = pkt[ARP].hwsrc
 		hostname = socket.gethostname()
 		my_ip = socket.gethostbyname(hostname)
-		my_mac = get_if_hwaddr('en0')
+		my_mac = get_if_hwaddr(ethernet_interface)
+		# debug -> my_mac = "11:11:11:11:11:11" -> to see ARP Reply pkts coming from host machine
 
 		print '\033[94m'+"--- ----------- ---------- ---"+'\033[0m'
 		print '\033[1m'+"Received ARP Reply IP "+ip_src+" - MAC "+mac_src+'\033[0m'
@@ -39,23 +40,28 @@ def arp_reply_intercept_esde(pkt):
 			#				no? Send and ARP broadcast to check for strong attacker
 			#					+2 responses? Strong attacker detected 
 			# 	No: weak attacker, fire alert
-
+			print "Two probe packets sent..."
+			if len(icmp_ans0) > 0:
+				print icmp_ans0[ICMP].summary()
+			if len(icmp_ans1) > 0:
+				print icmp_ans1[ICMP].summary()
 			# Keep checking if we get a response from the first ICMP echo request
 			# or if we don't get a response for the first one, but we do for the next one
-			if len(icmp_ans0) > 0 or (len(icmp_ans0) == 0 and len(icmp_ans1) > 0):
+			if (len(icmp_ans0) > 0) or (len(icmp_ans0) == 0 and len(icmp_ans1) > 0):
 				# Send another icmp message with our IP instead
 				print "Sending ICMP Packet with my IP."
 				icmp_ans, icmp_unans = srp(Ether(dst=mac_src)/IP(dst=my_ip)/ICMP(), timeout=2)
 				if len(icmp_ans) > 0:
 					# Intermediate attacker detected, fire alert
-					print "ALERT : Intermediate attacker detected!"
-					pkt.sprintf("Attacking MAC Addr:%ARP.hwsrc%")
+					print '\033[91m'+"ALERT : Intermediate attacker detected!"+'\033[0m'
+					print "Attacking MAC Addr: "+mac_src
 				else:
 					# If it all looks good, we send an ARP Request
 					# If we get more than one reply, we know there is a strong attacker
 					arp_ans, arp_unans = srp(Ether(dst="FF:FF:FF:FF:FF:FF")/ARP(pdst=ip_src))
-					if len(arp_ans) > 1:
-						print "ALERT : Strong attacker detected!"
+					print arp_ans[ARP].summary()
+					if arp_ans[0][1].hwsrc != mac_src:
+						print '\033[91m'+"ALERT : Strong attacker detected!"+'\033[0m'
 						print "Attacker MAC Addr cannot be determined."
 			else:
 				# If neither echo message was successful, assume it's an attacker
@@ -65,53 +71,8 @@ def arp_reply_intercept_esde(pkt):
 		print '\033[94m'+"--- ----------- ---------- ---"+'\033[0m'
 
 
+# Set up ethernet interface
+ethernet_interface = "en0"
 
-
-# Function for Secondary Cache solution
-def arp_reply_intercept_sec_cache(pkt):
-	# send an ICMP echo msg to the MAC that sent the ARP reply
-	if pkt[ARP].op == 2:
-		ip_src = pkt[ARP].psrc
-		mac_scr = pkt[ARP].hwsrc
-		# send ICMP packet
-		icmp_ans, icmp_unans = srp(Ether(dst=mac_src)/IP(dst=ip_src)/ICMP())
-		# if no response, update ARP table to previous entry
-		# otherwise, update secondary cache with new mapping
-		if len(icmp_ans) > 0:	
-			# update secondary cache
-			# secondary cache: key = IP, value = MAC
-			icmp_cache[mac_src] = ip_src
-		else:
-			# Alert about the attacker
-			print "ALERT : Weak attacker detected!"
-			pkt.sprintf("Attacking MAC Addr: %ARP.hwsrc%")
-			print "Adding static entry to ARP table..."
-			print "Please enter admin password when prompted."
-			# Get old mappings from cache
-			old_ip = icmp_cache[mac_src]
-			old_mac = mac_src
-			# Adding static entry
-			subprocess.check_call(["sudo", "arp", "-s", old_ip, old_mac])
-			print "Done updating ARP table."
-
-
-# Setup Secondary Cache
-# 1. Get IP-MAC bindings from current ARP cache table
-#icmp_cache
-# [router 	10.0.0.1   	00:50:f1:80:00:00]
-# [phone	10.0.0.224 	B8:D7:AF:B1:F0:59]
-# [bigMAC	10.0.0.150 	78:31:C1:C1:3B:72]
-# [miniMAC	10.0.0.109 	80:E6:50:0C:F7:C4]
-router_ip = '10.0.0.1'
-router_mac = '00:50:f1:80:00:00'
-
-attacker_ip = '10.0.0.150'
-attacker_mac = '78:31:C1:C1:3B:72'
-
-victim_ip = '10.0.0.109'
-victim_mac = '80:E6:50:0C:F7:C4'
-
-icmp_cache = dict(zip([router_mac,attacker_mac,victim_mac], [router_ip,attacker_ip,victim_ip]))
-
-# Get arp packets
-sniff(iface="en0", filter="arp", store=0, prn=arp_reply_intercept_esde)
+# Sniff ARP packets
+sniff(iface=ethernet_interface, filter="arp", store=0, prn=arp_reply_intercept_esde)
--- icmp_sec_cache.py
+++ icmp_sec_cache.py
@@ -3,11 +3,11 @@
 # Date:   11/22/2017 
 # ------------------------
 #
-# Ts solution is based on the algorithm developed on the paper:
+# This solution is based on the algorithm developed on the paper:
 # Nikhil Tripathi, BM Mehtre, 
-# “An ICMP based secondary cache approach for the detection and preventio of ARP poisoning” in 4th IEEE International Conference on Computational Intelligence and Computing Research (ICCIC). 
+# An ICMP based secondary cache approach for the detection and preventio of ARP poisoning.
+# 4th IEEE International Conference on Computational Intelligence and Computing Research (ICCIC). 
 # Publication year: 2013,
-
 
 # imports
 import socket
@@ -15,77 +15,61 @@ from scapy.all import *
 import subprocess
 
 
-# Function for E-SDE soltuion
-def arp_reply_intercept_esde(pkt):
-	# do soemthing if packet is a reply
-	if pkt[ARP].op == 2:
-		ip_src = pkt[ARP].psrc
-		mac_src = pkt[ARP].hwsrc
-		# send ICMP packet with new IP to MAC specified
-		icmp_ans, icmp_unans = srp(Ether(pdst=mac_src)/IP(dst=ip_scr)/ICMP())
-		# response?
-		#	Yes: send an ICMP message with our IP instead to MAC
-		#		Response: 	yes? intermediate attacker, fire alert
-		#				no? leave entry
-		# 	No: weak attacker, fire alert
-		if icmp_ans.summary() != "":
-			# Send another icmp message with our IP instead
-			hostname = socket.gethostname()
-			my_ip = socket.gethostbyname(hostname)
-			icmp_ans, icmp_unans = srp(Ether(pdst=mac_scr)/IP(dst=my_ip)/ICMP())
-			if icmp_ans != "":
-				# Intermediate attacker detected, fire alert
-				print "ALERT : Intermediate attacker detected!"
-				pkt.sprintf("Attacking MAC Addr:%ARP.hwsrc%")
-		else:
-			# Weak attacker detected, fire alert
-			print "ALERT : Weak attacker detected!"
-			pkt.sprintf("Attacking MAC Addr:%ARP.hwsrc%")
-
-
-
 # Function for Secondary Cache solution
 def arp_reply_intercept_sec_cache(pkt):
 	# send an ICMP echo msg to the MAC that sent the ARP reply
 	if pkt[ARP].op == 2:
 		ip_src = pkt[ARP].psrc
-		mac_scr = pkt[ARP].hwsrc
-		# send ICMP packet
-		icmp_ans, icmp_unans = srp(Ether(pdst=mac_src)/IP(dst=ip_src)/ICMP())
-		# if no response, update ARP table to previous entry
-		# otherwise, update secondary cache with new mapping
-		if icmp_ans.summary() != "":	
-			# update secondary cache
-			# secondary cache: key = IP, value = MAC
-			icmp_cache[mac_src] = ip_src
-		else:
-			# Alert about the attacker
-			print "ALERT : Weak attacker detected!"
-			pkt.sprintf("Attacking MAC Addr: %ARP.hwsrc%")
-			print "Adding static entry to ARP table..."
-			print "Please enter admin password when prompted."
-			# Get old mappings from cache
-			old_ip = icmp_cache[mac_src]
-			old_mac = mac_src
-			# Adding static entry
-			subprocess.check_call(["sudo", "arp", "-s", old_ip, old_mac])
-			print "Done updating ARP table."
+		mac_src = pkt[ARP].hwsrc
+		hostname = socket.gethostname()
+		my_ip = socket.gethostbyname(hostname)
+		my_mac = get_if_hwaddr(ethernet_interface)
+		# debug -> my_mac = "11:11:11:11:11:11" -> to see ARP Reply pkts coming from host machine
+
+		print '\033[94m'+"--- ----------- ---------- ---"+'\033[0m'
+		print '\033[1m'+"Received ARP Reply IP "+ip_src+" - MAC "+mac_src+'\033[0m'
+
+		# Check for attacker if the ARP Reply wasn't sent by this host
+		if mac_src != my_mac:
+			# send ICMP packet
+			icmp_ans, icmp_unans = srp(Ether(dst=mac_src)/IP(dst=ip_src)/ICMP())
+			# if no response, update ARP table to previous entry
+			# otherwise, update secondary cache with new mapping
+			if len(icmp_ans) > 0:	
+				# update secondary cache
+				# secondary cache: key = IP, value = MAC
+				icmp_cache[mac_src] = ip_src
+			else:
+				# Alert about the attacker
+				print '\033[91m'+"ALERT : Weak attacker detected!"+'\033[0m'
+				print "Attacking MAC Addr: "+mac_src
+				print "Adding static entry to ARP table..."
+				print "Please enter admin password when prompted."
+				# Get old mappings from cache
+				old_ip = icmp_cache[mac_src]
+				old_mac = mac_src
+				# Adding static entry
+				subprocess.check_call(["sudo", "arp", "-s", old_ip, old_mac])
+				print "Done updating ARP table."
+		print '\033[94m'+"--- ----------- ---------- ---"+'\033[0m'
+
 
 
 # Setup Secondary Cache
-# 1. Get IP-MAC bindings from current ARP cache table
-#icmp_cache
-# [router 	10.0.0.1   	00:50:f1:80:00:00]
-# [phone	10.0.0.224 	B8:D7:AF:B1:F0:59]
-# [miniMAC	10.0.0.151 	78:31:C1:C1:3B:72]
-# [bigMAC	10.0.0.109 	80:E6:50:0C:F7:C4]
+# Set IP-MAC bindings from current ARP cache table
+print "Setting up Secondary Cache..."
 router_ip = '10.0.0.1'
 router_mac = '00:50:f1:80:00:00'
 
-attacker_ip = '10.0.0.151'
-attacker_mac = '78:31:C1:C1:3B:72'
+victim_ip = '10.0.0.150'
+victim_mac = '78:31:C1:C1:3B:72'
 
-icmp_cache = dict(zip([router_mac,attacker_mac], [router_ip,attacker_ip]))
+attacker_ip = '10.0.0.109'
+attacker_mac = '80:E6:50:0C:F7:C4'
+
+icmp_cache = dict(zip([router_mac,attacker_mac,victim_mac], [router_ip,attacker_ip,victim_ip]))
+
+ethernet_interface = "en0"
 
 # Get arp packets
-sniff(iface="en0", filter="arp", store=0, prn=arp_reply_intercept_sec_cache)
+sniff(iface=ethernet_interface, filter="arp", store=0, prn=arp_reply_intercept_sec_cache)
